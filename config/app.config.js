b081ecb265b399ac4e38aeefa4119b4737e5b27a
mert-solak <solak.mert@hotmail.com>
2021-05-16 21:12:10 +0300

    fix(.github): workflow added

--- config/app.config.js
+++ config/app.config.js
@@ -11,7 +11,7 @@ module.exports = {
   },
   production: {
     remotes: {
-      'remote/task': `task@${domain}/template/remoteEntry.js`,
+      task: `task@${domain}/task/remoteEntry.js`,
     },
   },
 };
